0690c9315ba3bf2cb201f7ca026a6b3ac3536ec6
Osanda <osanda.v13@gmail.com>
2023-04-01 16:56:08 +0530

    air pollution model and structure added

---
+++ CourseWork2Starter/AirModel/PollutionDataStructure.swift
@@ -0,0 +1,9 @@
+//
+//  PollutionDataStructure.swift
+//  CourseWork2Starter
+//
+//  Created by Osanda Viduda on 2023-04-01.
+//
+
+import Foundation
+
--- CourseWork2Starter/AirViews/PollutionView.swift
+++ CourseWork2Starter/AirViews/PollutionView.swift
@@ -9,29 +9,32 @@ import SwiftUI
 
 struct PollutionView: View {
     
-    // @EnvironmentObject and @State varaibles here
+    //@EnvironmentObject airModel:
 
     var body: some View {
         
         ZStack {
-
             // Use ZStack for background images
-            
+            Image("background").resizable().ignoresSafeArea()
             VStack {
                 Text("")
                 Spacer()
                 Text("This is a mixed view with weather \n and air quality data,\n refer to Figure 5 to see what data must be displayed here")
                 Spacer()
 
-                    }
-                    
+            }.foregroundColor(.black)
+            .shadow(color: .black,  radius: 0.5)
                 
-                .foregroundColor(.black)
-                .shadow(color: .black,  radius: 0.5)
-                
-            }.ignoresSafeArea(edges: [.top, .trailing, .leading])
-        }
+        }.ignoresSafeArea(edges: [.top, .trailing, .leading])
     }
+}
+
+
+struct PollutionPreviews: PreviewProvider {
+    static var previews: some View {
+        PollutionView().environmentObject(ModelData())
+    }
+}
 
     
 
