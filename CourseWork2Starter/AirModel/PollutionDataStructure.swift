67563968ece864f98bb61c4a113ae7b85bb0b837
Osanda <osanda.v13@gmail.com>
2023-04-02 11:27:19 +0530

    json data structure added

--- CourseWork2Starter/AirModel/PollutionDataStructure.swift
+++ CourseWork2Starter/AirModel/PollutionDataStructure.swift
@@ -7,3 +7,50 @@
 
 import Foundation
 
+struct Pollution: Codable {
+    let coord:[Double]
+    let list:[List]
+}
+
+struct List: Codable {
+    let dt:Int
+    let info: Info
+    let components: Components
+    
+    enum CodingKeys:String, CodingKey {
+        case dt
+        case info = "main"
+        case components
+    }
+}
+
+struct Info: Codable {
+    let airQulityIndex:String
+    
+    enum Codingkeys:String, CodingKey {
+        case airQulityIndex = "aqi"
+    }
+}
+
+struct Components:Codable {
+    let carbonMonoxide:String
+    let nitrogenMonoxide:String
+    let nitrogenDioxide:String
+    let ozone:String
+    let sulphurDioxide:String
+    let FineParticlesMatter:String
+    let CoarseParticulateMatter:String
+    let ammonia:String
+
+
+    enum CodingKeys:String, CodingKey{
+        case carbonMonoxide = "co"
+        case nitrogenMonoxide = "no"
+        case nitrogenDioxide = "no2"
+        case ozone = "o3"
+        case sulphurDioxide = "so2"
+        case FineParticlesMatter = "pm2_5"
+        case CoarseParticulateMatter = "pm10"
+        case ammonia = "nh3"
+    }
+}
