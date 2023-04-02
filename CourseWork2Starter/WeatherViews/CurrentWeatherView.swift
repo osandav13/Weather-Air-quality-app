0bb3a268002d21e566ccb371f22f8b1dfbd47c40
Osanda <osanda.v13@gmail.com>
2023-04-02 17:56:49 +0530

    CurrentWeatherView updated

--- CourseWork2Starter/WeatherViews/CurrentWeatherView.swift
+++ CourseWork2Starter/WeatherViews/CurrentWeatherView.swift
@@ -16,14 +16,14 @@ struct CurrentWeatherView: View {
         ZStack {
             Image("background2").resizable()
         
-            VStack {
+            VStack (spacing:15){
                 Text("\(locationString)")
                     .font(.largeTitle)
                     .fontWeight(.semibold)
                     .multilineTextAlignment(.center)
                     .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
 
-                VStack{
+                VStack (spacing:40){
 
         //          Temperature Info
                     
@@ -32,30 +32,55 @@ struct CurrentWeatherView: View {
                             .padding()
                             .font(.largeTitle)
                         HStack {
-
+                            WeatherIcon(icon: modelData.forecast!.current.weather[0].icon)
                             Text(modelData.forecast!.current.weather[0].weatherDescription.rawValue.capitalized)
                                 .foregroundColor(.black)
                         }
-
-                        Text("Feels Like: \((Int)(modelData.forecast!.current.feelsLike))ºC")
-                            .foregroundColor(.black)
-                    }.padding()
+                        VStack (spacing:20){
+                            HStack(spacing:40){
+                                Text("High: \((Int)(modelData.forecast!.daily[0].temp.max))")
+                                Text("Low: \((Int)(modelData.forecast!.daily[0].temp.max))")
+                            }
+                            Text("Feels Like: \((Int)(modelData.forecast!.current.feelsLike))ºC")
+                                .foregroundColor(.black)
+                        }
+                        
+                    }.padding(30)
                     
- 
+                    VStack(spacing:50){
+                        HStack(spacing:60){
+                            Text("Wind Speed: \((Int)(modelData.forecast!.daily[0].windSpeed)) m/s")
+                            Text("Direction: \(convertDegToCardinal(deg: modelData.forecast!.daily[0].windDeg))")
 
+                        }
+                        HStack(spacing:50){
+                            Text("Humidity: \((Int)(modelData.forecast!.daily[0].humidity))%")
+                            Text("Presure: \(modelData.forecast!.daily[0].pressure) hPg")
+
+                        }
+                    }
+                    
+                    
+                }.padding(.bottom)
+
+                HStack(spacing:10){
+                    Image(systemName: "sunset.fill").foregroundColor(.yellow)
+                    Text(Date(timeIntervalSince1970: Double(modelData.forecast?.current.sunset ?? 0)).formatted(.dateTime.hour().minute()))
+                    Image(systemName: "sunrise.fill").foregroundColor(.yellow)
+                    Text(Date(timeIntervalSince1970: Double(modelData.forecast!.current.sunrise ?? 0)).formatted(.dateTime.hour().minute()))
                 }
-                
             }
             .foregroundColor(.black)
             .shadow(color: .black,  radius: 0.5)
             
-        }.ignoresSafeArea()
-            .onAppear{
-                Task.init {
-                    self.locationString = await getLocFromLatLong(lat: modelData.forecast!.lat, lon: modelData.forecast!.lon)
-                    
-                }
+        }
+        .ignoresSafeArea()
+        .onAppear{
+            Task.init {
+                self.locationString = await getLocFromLatLong(lat: modelData.forecast!.lat, lon: modelData.forecast!.lon)
+                
             }
+        }
     }
 }
 
